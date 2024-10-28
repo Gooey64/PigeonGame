8f6f5380ce73b1874c7b88a6d130ce7a2f2bc0ad
APrince26 <138528286+APrince26@users.noreply.github.com>
2024-10-27 23:50:18 -0400

    final update to menu

---
+++ pigeonProject/Assets/LevelMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class LevelMenu : MonoBehaviour
+{
+    public Button[] buttons;
+
+    private void Awake()
+    {
+        int unlockedlevel = PlayerPrefs.GetInt("UnlockedLevel1", 1);
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            buttons[i].interactable = false;
+        }
+        for (int i = 0; i < unlockedlevel; i++) 
+        {
+            buttons[i].interactable = true;
+        }
+    }
+   public void OpenLevel(int levelID) 
+   {
+        string levelName = "Level " + levelID;
+        SceneManager.LoadScene(levelName);
+   }
+}
+  
