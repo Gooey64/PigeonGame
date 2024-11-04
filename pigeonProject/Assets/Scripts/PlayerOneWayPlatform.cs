0db05e9fe38acaea4abd4845270579482863acce
Gooey64 <61029550+Gooey64@users.noreply.github.com>
2024-11-04 14:22:13 -0500

    Tried to add pigeon platform
    since it doesn't work yet, I created a branch, but basically the pigeon should be able to jump on top of the platform and then not fall through, but rn it just falls through the platform

---
+++ pigeonProject/Assets/Scripts/PlayerOneWayPlatform.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private GameObject currentOneWayPlatform;
+
+    [SerializeField] private BoxCollider2D playerCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("OneWayPlatform"))
+        {
+            currentOneWayPlatform = collision.gameObject;   
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("OneWayPlatform"))
+        {
+            currentOneWayPlatform = null;   
+        }
+    }
+
+    private IEnumerator DisableCollision()
+    {
+        BoxCollider2D platformCollider = currentOneWayPlatform.GetComponent<BoxCollider2D>();
+
+        Physics2D.IgnoreCollision(playerCollider, platformCollider);
+        yield return new WaitForSeconds(0.25f);
+        Physics2D.IgnoreCollision(playerCollider, platformCollider, false);
+    }
+}
