de244fead55c0d881fc099855b3d0cc49b91c40a
cza17 <Chenxi.Zhang@tufts.edu>
2024-11-26 22:21:00 -0500

    Revert "changed the options panel with Stephanie"
    This reverts commit f1a4703658ae9e68184f6b8e17e9bd64cc7103cd.

--- pigeonProject/Assets/Scripts/SoundMixerManager.cs
+++ pigeonProject/Assets/Scripts/SoundMixerManager.cs
@@ -2,34 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
-using UnityEngine.UI;
 
 public class SoundMixerManager : MonoBehaviour
 {
    [SerializeField] private AudioMixer audioMixer;
-    private Slider volumeCtrl;
-    public static float volumeLvl = 1.0f;
 
-    private void Awake()
-    {
-        SetMasterVolume(volumeLvl);
-    }
-
-    public void SetMasterVolume(float level)
-    {
+   public void SetMasterVolume(float level) 
+   {
         audioMixer.SetFloat("masterVolume", Mathf.Log10(level) * 20f);
-        volumeLvl = level;
-    }
+   }
 
-    public void SetSoundFXVolume(float level)
-    {
+   public void SetSoundFXVolume(float level)
+   {
         audioMixer.SetFloat("soundFXVolume", Mathf.Log10(level) * 20f);
-        volumeLvl = level;
-    }
+   }
 
     public void SetMusicVolume(float level)
     {
         audioMixer.SetFloat("musicVolume", Mathf.Log10(level) * 20f);
-        volumeLvl = level;
     }
 }
