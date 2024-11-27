f1a4703658ae9e68184f6b8e17e9bd64cc7103cd
cza17 <Chenxi.Zhang@tufts.edu>
2024-11-26 22:01:49 -0500

    changed the options panel with Stephanie

--- pigeonProject/Assets/Scripts/SoundMixerManager.cs
+++ pigeonProject/Assets/Scripts/SoundMixerManager.cs
@@ -2,23 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SoundMixerManager : MonoBehaviour
 {
    [SerializeField] private AudioMixer audioMixer;
+    private Slider volumeCtrl;
+    public static float volumeLvl = 1.0f;
 
-   public void SetMasterVolume(float level) 
-   {
+    private void Awake()
+    {
+        SetMasterVolume(volumeLvl);
+    }
+
+    public void SetMasterVolume(float level)
+    {
         audioMixer.SetFloat("masterVolume", Mathf.Log10(level) * 20f);
-   }
+        volumeLvl = level;
+    }
 
-   public void SetSoundFXVolume(float level)
-   {
+    public void SetSoundFXVolume(float level)
+    {
         audioMixer.SetFloat("soundFXVolume", Mathf.Log10(level) * 20f);
-   }
+        volumeLvl = level;
+    }
 
     public void SetMusicVolume(float level)
     {
         audioMixer.SetFloat("musicVolume", Mathf.Log10(level) * 20f);
+        volumeLvl = level;
     }
 }
