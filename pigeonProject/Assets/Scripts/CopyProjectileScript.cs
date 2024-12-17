1977b9ea216a7d60e0d5f13a297e3695332cfe76
APrince26 <138528286+APrince26@users.noreply.github.com>
2024-12-17 14:40:47 -0500

    updates

---
+++ pigeonProject/Assets/Scripts/CopyProjectileScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyProjectileScript : MonoBehaviour
+{
+    [SerializeField] private AudioClip Santa;
+    public float damage = 10f;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) 
+        {
+            Debug.Log("Projectile hit the pigeon!");
+
+            HealthManager healthManager = other.GetComponent<HealthManager>();
+            if (healthManager != null)
+            {
+                SoundFXManager.instance.PlaySoundFXClip(Santa);
+                healthManager.TakeDamage(damage);
+            }
+
+            Destroy(gameObject);
+        }
+    }
+}
